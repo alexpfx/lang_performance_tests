b8aa37911acc4a4ae99b7b19e17f7344b6efac10
alexpfx <alexpfba@gmail.com>
2022-05-07 16:08:36 -0300

    filter

--- java/microbenchmarks/src/main/java/org/example/Main.java
+++ java/microbenchmarks/src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
-import benchmarks.DefaultExecutor;
-import benchmarks.Executor;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import benchmarks.DefaultExecutor;
+import benchmarks.Executor;
 
 public class Main {
     List<DefaultExecutor> tests = new ArrayList<>();
@@ -24,15 +25,33 @@ public class Main {
                     return sorted.findFirst().get();
                 });
 
+        final DefaultExecutor streamFilter = DefaultExecutor.newDefaultExecutor("t2 stream filter",
+                (numbers, index) -> {
+                    List<Integer> l = numbers.stream().filter(n -> n > 10000 && n < 10005).collect(Collectors.toList());
+                    return l.size();
+                });
+
+        final DefaultExecutor normalFilter = DefaultExecutor.newDefaultExecutor("t2 stream filter",
+                (numbers, index) -> {
+                    List<Integer> l = new ArrayList<>();
+                    for (Integer n : numbers) {
+                        if (n > 10000 && n < 10005) {
+                            l.add(n);
+                        }
+                    }
+                    return l.size();
+                });
+
         tests.add(streamSort);
+        tests.add(streamFilter);
+        tests.add(normalFilter);
     }
 
-    public void execute(){
+    public void execute() {
         for (DefaultExecutor ex : tests) {
             final Executor.Summary summary = ex.execute();
             System.out.println(summary);
         }
     }
-
 
 }
