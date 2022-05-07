4031a4fb2034acdf71125f0385629eeaa930bf06
alexpfx <alexpfba@gmail.com>
2022-05-07 18:02:10 -0300

    shuffle removido

--- java/microbenchmarks/src/main/java/benchmarks/Executor.java
+++ java/microbenchmarks/src/main/java/benchmarks/Executor.java
@@ -52,13 +52,13 @@ public abstract class Executor<T extends Task<List<Integer>, Integer>, G extends
         Summary s = new Summary();
         s.title = testName;
         Instant start = Instant.now();
-
+        System.out.println("gen data");
         final List<Integer> data = generator.genData();
+        System.out.println("data generated");
         
         List<Integer> trash = new ArrayList<>();
         List<Duration> times = new ArrayList<>();
         for (int i = 0; i < executions; i++) {
-
             Instant b = Instant.now();
             trash.add(task.process(data, i));
             times.add(Duration.between(b, Instant.now()));
